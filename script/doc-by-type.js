239ff41d154250e9778e699fda73ed8c8880f59e
qefeng <qefeng@tencent.com>
2020-11-19 11:58:58 +0800

    feat: 生成类型描述 schema

--- script/doc-by-type.js
+++ script/doc-by-type.js
@@ -1,5 +1,20 @@
-const yargs = require("yargs/yargs");
-const { hideBin } = require("yargs/helpers");
-const TJS = require("typescript-json-schema");
+/** node script/doc-by-type.js --file-path <type-export-file> --type-name <type-name or *> --file-root <optional-file-root> */
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const TJS = require('typescript-json-schema');
 
-const { typeRoot, typeDesc } = yargs(hideBin(process.argv)).argv;
+const { filePath, fileRoot, typeName } = yargs(hideBin(process.argv)).argv;
+
+const program = TJS.getProgramFromFiles(
+  [filePath],
+  {
+    ignoreErrors: true,
+  },
+  fileRoot
+);
+
+const schema = TJS.generateSchema(program, typeName, {
+  ignoreErrors: true,
+});
+
+console.log(schema);
