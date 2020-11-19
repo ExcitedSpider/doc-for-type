3121b1aac868d92037ce69c31a4ccd1718f4390e
qefeng <qefeng@tencent.com>
2020-11-19 11:24:37 +0800

    remove junk

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/4f38ab4e4a08940d3a5d81db215c3db0.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from 'docz';
-import { Alert } from "../../../../../src/components/Alert.tsx";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  Alert,
-  React
-};
--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/e09dad8af46eb5b1f11a094231d8e8a6.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from 'docz';
-import { Button } from "../../../../../src/components/Button.tsx";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  Button,
-  React
-};
